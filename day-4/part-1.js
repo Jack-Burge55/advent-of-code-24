ae49c3f4845779d4c01e6250c5bbc4f4ddb7b906
Jack Burgess <jack.burgess412@gmail.com>
2024-12-04 09:16:40 +0000

    lint

--- day-4/part-1.js
+++ day-4/part-1.js
@@ -3,35 +3,52 @@ let data;
 
 try {
   // get data in 140x140 array x array
-  data = fs.readFileSync("input.txt", "utf8").split("\r\n").map(line => line.split(""));
+  data = fs
+    .readFileSync("input.txt", "utf8")
+    .split("\r\n")
+    .map((line) => line.split(""));
 } catch (err) {
   console.error("Error reading the file:", err);
 }
 
-let xmasCount = 0
-const gridWidth = 140
+let xmasCount = 0;
+const gridWidth = 140;
 
 const checkForWord = (word) => {
-  if (word === "XMAS" || word === "SAMX") xmasCount++
-}
+  if (word === "XMAS" || word === "SAMX") xmasCount++;
+};
 
 for (let i = 0; i < gridWidth; i++) {
   for (let j = 0; j < gridWidth; j++) {
     // up-right diagonal case
     if (i >= 3 && j <= gridWidth - 4) {
-      checkForWord(data[i][j] + data[i-1][j+1] + data[i-2][j+2] + data[i-3][j+3])
+      checkForWord(
+        data[i][j] +
+          data[i - 1][j + 1] +
+          data[i - 2][j + 2] +
+          data[i - 3][j + 3]
+      );
     }
     // right case
     if (j <= gridWidth - 4) {
-      checkForWord(data[i][j] + data[i][j+1] + data[i][j+2] + data[i][j+3])
+      checkForWord(
+        data[i][j] + data[i][j + 1] + data[i][j + 2] + data[i][j + 3]
+      );
     }
     // down-right case
     if (i <= gridWidth - 4 && j <= gridWidth - 4) {
-      checkForWord(data[i][j] + data[i+1][j+1] + data[i+2][j+2] + data[i+3][j+3])
+      checkForWord(
+        data[i][j] +
+          data[i + 1][j + 1] +
+          data[i + 2][j + 2] +
+          data[i + 3][j + 3]
+      );
     }
     // down case
     if (i <= gridWidth - 4) {
-      checkForWord(data[i][j] + data[i+1][j] + data[i+2][j] + data[i+3][j])
+      checkForWord(
+        data[i][j] + data[i + 1][j] + data[i + 2][j] + data[i + 3][j]
+      );
     }
   }
 }
